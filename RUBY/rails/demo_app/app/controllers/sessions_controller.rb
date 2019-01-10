e23369ef61f7bf6fb5651947baea1f79b66b633b
lap14tclc2 <honaz140@gmail.com>
2019-01-10 09:15:18 +0700

    add helper session, show links when user login and config routes

--- RUBY/rails/demo_app/app/controllers/sessions_controller.rb
+++ RUBY/rails/demo_app/app/controllers/sessions_controller.rb
@@ -18,5 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
--- RUBY/rails/demo_app/app/helpers/sessions_helper.rb
+++ RUBY/rails/demo_app/app/helpers/sessions_helper.rb
@@ -2,4 +2,28 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  # set current user
+  def current_user
+    # check if user is existed in session
+    if (user_id = session[:user_id])
+      @current_user ||= User.find(user_id)
+    end
+  end
+
+  #check user is current user?
+  def current_user?(user)
+    user == current_user
+  end
+
+  # if current user is existed so he logged in
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
 end
